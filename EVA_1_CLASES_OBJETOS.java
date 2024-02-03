1a0f5fdf42f6900e214e8c171733e720b3110c5c
KevinDanielGardeaSan <158379717+KevinDanielGardeaSan@users.noreply.github.com>
2024-02-02 18:57:33 -0600

    Add files via upload

---
+++ EVA_1_CLASES_OBJETOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_clases_objetos;
+
+/**
+ *
+ * @author nacho
+ */
+public class EVA_1_CLASES_OBJETOS {
+
+    public static void main(String[] args) {
+    Person persol: //declaro un identifiasor (variable) de tipo persona
+    //solo he declarado la variable, no he creado el objeto
+    //Crear el objeto (instanciar)consiste en asignar memoria 
+    int valor:
+    }
+    
+}
+//
+class PERSONA 
+        /*NombreDeLaClase --> Nomenclatura Came1Case
+        Class NombreDeLaClase(cuerpo de la clase )
+        
+        */
+{
+
+}
