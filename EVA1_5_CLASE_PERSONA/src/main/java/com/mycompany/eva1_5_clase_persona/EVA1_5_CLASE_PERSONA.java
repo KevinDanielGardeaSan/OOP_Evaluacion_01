92dc4ccd3e42608d4ab1daf5cdd5b8795a24b73b
KevinDanielGardeaSan <158379717+KevinDanielGardeaSan@users.noreply.github.com>
2024-02-10 21:59:06 -0600

    Add files via upload

---
+++ EVA1_5_CLASE_PERSONA/src/main/java/com/mycompany/eva1_5_clase_persona/EVA1_5_CLASE_PERSONA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_5_clase_persona;
+
+/**
+ *
+ * @author nacho
+ */
+public class EVA1_5_CLASE_PERSONA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
